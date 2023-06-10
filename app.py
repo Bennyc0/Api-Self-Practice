64b18dbea4a2c49bdb20c9cf267b6647c76578fc
Bennyc0 <benny.chen@csedge.org>
2023-06-09 22:12:53 -0400

    login/signup/logout features working as intended

--- app.py
+++ app.py
@@ -1,33 +1,127 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 
+import database_functions as dbf
+
 app = Flask(__name__)
+current_user = ""
+current_email = ""
 
+# ---------- Homepage ----------
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    global current_user
 
+    welcome_message = ""
+
+    if current_user != "":
+        welcome_message = f"Hello there {current_user}! Welcome to Simple Pokedex"
+
+    return render_template("home.html", welcome_message=welcome_message)
+
+
+# ---------- Login/Signup ----------
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/verify-user", methods=['GET', 'POST'])
+def verify_user():
+    global current_user
+    global current_email
+
+    username = ""
+    email = request.form['email']
+    password = request.form['password']
+
+    current_user = dbf.verify_user(email, password)
+
+    if current_user != "":
+        current_email = email
+
+        return redirect(url_for('home'))
+    else:
+        return render_template('login.html', message='Invalid Gmail or Password, Please Try Again')
+
+
+@app.route('/store-user', methods=['GET', 'POST'])
+def store_user():
+    global current_user
+    global current_email
+
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
+
+    if dbf.get_user_rowid(email) == "":
+        dbf.signup_user(username, email, password)
+
+        current_user = username
+        current_email = email
+
+        return redirect(url_for('home'))
+
+    else:
+        return render_template('login.html', message="Email Already In Use, Log In Instead?")
+
+
+@app.route('/logout')
+def logout():
+    global current_user
+    global current_email
+
+    current_user = ""
+    current_email = ""
+
+    return render_template('login.html', message='Successfully Logged Out!')
+
+# ---------- Loadouts ----------
+@app.route("/loadouts")
+def loadouts():
+    global current_user
+    global current_email
+
+    user_rowid = dbf.get_user_rowid(current_email)
+    user_loadouts = dbf.find_user_loadouts(user_rowid)
+
+    return render_template('loadouts.html', loadouts=user_loadouts)
+
+# ===== Create/Edit/Delete In Development =====
+@app.route("/new-loadout")
+
+@app.route("/edit-loadout")
+
+@app.route("/delete-loadout")
+
+# ---------- Search Result ----------
 @app.route("/search-result", methods=['GET', 'POST'])
 def search_result():
     search_name = request.form["input"].lower().strip()
+    search_name = search_name.replace(" ", "-")
 
     try:
         data = requests.get(f"https://pokeapi.co/api/v2/pokemon/{search_name}").json()
-        testing = data['name']
     except :
-        data = requests.get(f"https://pokeapi.co/api/v2/pokemon/bulbasaur").json()
+        return render_template('index.html', message="No Search Results Found, Check Your Spelling")
 
     # Empty Lists
     lists = [
         type_list := [],
         ability_list := [],
         uneffective_list := [],
-        temp_weakness_list := [],
         weakness_list := [],
         immunity_list := []
     ]
 
+    # Empty Lists Used For Processing
     hidden_lists = [
+        temp_weakness_list := [],
         advantage_list := []
     ]
 
@@ -65,51 +159,31 @@ def search_result():
         if not (weakness in type_list or weakness in immunity_list or weakness in advantage_list or weakness in weakness_list):
             weakness_list.append(weakness)
             
+    # Removing Dashes in Certain Pokemon Names
+    pkmn_name = data['name'].title().replace("-", " ")
 
     # Put Information Together
     pkmn_info = {
-        "Name" : data['name'].capitalize(),
+        "Name" : pkmn_name,
         "Pokedex ID" : str(data['id']),
         "Types" : type_list,
         "Abilities": ", ".join(ability_list),
-        "Weak to" : ", ".join(weakness_list),
+        "Weak to" : weakness_list,
     }
 
     # Removing Pokedex ID For Special Forms
-    if int(pkmn_info.get("Pokedex ID")) > 1008:
+    if int(pkmn_info.get("Pokedex ID")) > 1010:
         pkmn_info.update({"Pokedex ID": "Unavailable"})
 
     # Adding Immunity List
     if len(immunity_list) > 0:
-        pkmn_info.update({"Immune to": ", ".join(immunity_list)})
+        pkmn_info.update({"Immune to": immunity_list})
 
     # Pokemon Images
     normal_sprite = data['sprites']['other']['official-artwork']['front_default']
     shiny_sprite = data['sprites']['other']['official-artwork']['front_shiny']
 
-    # Pokemon Type Colors
-    type_colors = {
-        "Normal": "A8A77A",
-        "Fire": "EE8130",
-        "Water": "6390F0",
-        "Electric": "F7D02C",
-        "Grass": "7AC74C",
-        "Ice": "96D9D6",
-        "Fighting": "C22E28",
-        "Poison": "A33EA1",
-        "Ground": "E2BF65",
-        "Flying": "A98FF3",
-        "Psychic": "F95587",
-        "Bug": "A6B91A",
-        "Rock": "B6A136",
-        "Ghost": "735797",
-        "Dragon": "6F35FC",
-        "Dark": "05746",
-        "Steel": "B7B7CE",
-        "Fairy": "D685AD" 
-    }
-
-    return render_template("search-result.html", pkmn_info=pkmn_info, normal_sprite=normal_sprite, shiny_sprite=shiny_sprite, type_colors=type_colors)
+    return render_template("search-result.html", pkmn_info=pkmn_info, normal_sprite=normal_sprite, shiny_sprite=shiny_sprite)
 
 if __name__== '__main__':
     app.run(debug=True, host='0.0.0.0', port='5000')
---
+++ database_functions.py
@@ -0,0 +1,94 @@
+import sqlite3
+
+database_link = "./static/data/database.db"
+
+# ---------- Login/Signup ----------
+# Verify User
+def verify_user(email, password):
+    connect = sqlite3.connect(database_link)
+    cursor = connect.cursor()
+
+    result = cursor.execute('SELECT * FROM userbase WHERE email = ? AND password = ?', (email, password,))
+        
+    information = {
+        "username": ""
+    }
+    
+    for item in result:
+        information = {
+            "username": item[0]
+        }
+
+    connect.close()
+    return information['username']
+
+# Sign Up
+def signup_user(username, email, password):
+    connect = sqlite3.connect(database_link)
+    cursor = connect.cursor()
+
+    cursor.execute('INSERT INTO userbase(username, email, password) VALUES (?, ?, ?)', (username, email, password,))
+
+    connect.commit()
+    connect.close()
+
+
+# ---------- Get User ----------
+# Finds User ID Associated With Email
+# Can Also Be Used To Prevent Duplicate Email (If Nothing Is Found, Returns Blank, Meaning Email Not In Use Yet)
+def get_user_rowid(email):
+    connect = sqlite3.connect(database_link)
+    cursor = connect.cursor()
+
+    result = cursor.execute('SELECT rowid FROM userbase WHERE email = ?', (email,))
+        
+    information = {
+        "rowid": ""
+    }
+    
+    for item in result:
+        information = {
+            "rowid": item[0]
+        }
+
+    connect.close()
+    return information['rowid']
+
+
+# ---------- Loadouts ----------
+# Uses User ID To Find Saved Loadouts
+def find_user_loadouts(rowid):
+    connect = sqlite3.connect(database_link)
+    cursor = connect.cursor()
+
+    result = cursor.execute('SELECT * FROM loadouts WHERE rowid = ?', (rowid,))
+    
+    loadouts = []
+
+    for item in result:
+        information = {
+            # item[0] returns rowid
+            "slot 1": item[1],
+            "slot 2": item[2],
+            "slot 3": item[3],
+            "slot 4": item[4],
+            "slot 5": item[5],
+            "slot 6": item[6]
+        }
+
+        loadouts.append(information)
+
+    connect.close()
+
+    return loadouts
+
+def save_loadout(email, loadout_list):
+    connect = sqlite3.connect(database_link)
+    cursor = connect.cursor()
+
+    user_rowid = get_user_rowid(email)
+
+    cursor.execute('INSERT INTO loadouts(user_id, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6) VALUES (?, ?, ?, ?, ?, ?, ?)', (user_rowid, loadout_list[0], loadout_list[1], loadout_list[2], loadout_list[3], loadout_list[4], loadout_list[5],))
+
+    connect.commit()
+    connect.close()
